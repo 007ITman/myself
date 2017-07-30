c4582553bcabd53ffa836c44e4e7b4e4f56f351d
zd <1324828410@qq.com>
2017-07-30 13:48:41 +0800

    增加一个新的版本，为了测试是否能从服务器上撤销提交的履历

--- Workframe/src/main/java/com/zd/wokframe/workframe/LoginCrew.java
+++ Workframe/src/main/java/com/zd/wokframe/workframe/LoginCrew.java
@@ -3,6 +3,6 @@ package com.zd.wokframe.workframe;
 public class LoginCrew extends LoginCrewAb implements LoginCrewInf{
 
 	public static void main(String[] args) {
-		
+		System.out.println("1.0");
 	}
 }
